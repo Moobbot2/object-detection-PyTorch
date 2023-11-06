5f6f137e16b632c055638813fad803be3ccfb6a2
Ngô ĐứcTâm <133946215+Moobbot2@users.noreply.github.com>
2023-11-06 11:03:50 +0700

    format code

--- src/config.py
+++ src/config.py
@@ -1,31 +1,31 @@
 import torch
 import os
 
-BATCH_SIZE = 8 #increase/ decrease according to GPU memory
-RESIZE_TD = [640,640] # resize the image for training and trainsforms
-NUM_EPOCHS = 100 #number epochs to train for
+BATCH_SIZE = 8  # increase/ decrease according to GPU memory
+RESIZE_TD = [640, 640]  # resize the image for training and trainsforms
+NUM_EPOCHS = 100  # number epochs to train for
 
-DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-print('DEVICE:', DEVICE)
+DEVICE = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+print("DEVICE:", DEVICE)
 
-ANNOTS_DIR ='./dataset/annots/label_train'
-IMAGES_DIR ='./dataset/images/pano_train'
+ANNOTS_DIR = "./dataset/annots/label_train"
+IMAGES_DIR = "./dataset/images/pano_train"
 SPLIT_RATIO = 0.3
-#classes: 0 index is reserved for background
-CLASSES = ['background','3','4','5']
+# classes: 0 index is reserved for background
+CLASSES = ["background", "3", "4", "5"]
 NUM_CLASSES = 4
 
-# whether to visualize images after crearing the data loaders
+# whether to visualize images after creating the data loaders
 VISUMLIZE_TRANSFORMED_IMAGES = False
 
 # location to save model and plots
-OUT_DIR = './outputs_new'
+OUT_DIR = "./outputs_new"
 
 # Type Image
-IMAGE_TYPE = ('.jpg', '.png', '.bmp')
+IMAGE_TYPE = (".jpg", ".png", ".bmp")
 
 if not os.path.isdir(OUT_DIR):
     os.makedirs(OUT_DIR)
 
-SAVE_PLOTS_EPOCH = 5 # Save loss plots after these many epochs
-SAVE_MODEL_EPOCH = 5 # Save model after these many epochs
+SAVE_PLOTS_EPOCH = 5  # Save loss plots after these many epochs
+SAVE_MODEL_EPOCH = 5  # Save model after these many epochs
--- src/datasets.py
+++ src/datasets.py
@@ -6,10 +6,12 @@ import glob as glob
 import matplotlib.pyplot as plt
 from xml.etree import ElementTree as et
 
-from config import CLASSES, RESIZE_TD, IMAGES_DIR, ANNOTS_DIR, BATCH_SIZE, SPLIT_RATIO, IMAGE_TYPE
+from config import (CLASSES, RESIZE_TD, IMAGES_DIR,
+                    ANNOTS_DIR, BATCH_SIZE, SPLIT_RATIO, IMAGE_TYPE)
 from torch.utils.data import Dataset, DataLoader
 from ultis import collate_fn, get_train_transform, get_valid_transform
 from sklearn.model_selection import train_test_split
+
 
 # The dataset class
 class CustDataset(Dataset):
@@ -24,41 +26,63 @@ class CustDataset(Dataset):
         self.all_same_names = self.prepare_all_same_names()
 
     def prepare_all_same_names(self):
-        print('--- Check intersection/incompatible---')
+        print("--- Check intersection/incompatible---")
         # Get image file names and remove extensions
-        image_name_files = [os.path.splitext(os.path.basename(image_file))[0] for image_file in self.list_images_path]
+        image_name_files = [
+            os.path.splitext(os.path.basename(image_file))[0]
+            for image_file in self.list_images_path
+        ]
 
         # Get the base names of annotation files
-        annot_files = [os.path.basename(fname) for fname in os.listdir(self.annots_dir) if fname.endswith('.xml')]
-        annot_name_files = [os.path.splitext(annot_file)[0] for annot_file in annot_files]
+        annot_files = [
+            os.path.basename(fname)
+            for fname in os.listdir(self.annots_dir)
+            if fname.endswith(".xml")
+        ]
+        annot_name_files = [
+            os.path.splitext(annot_file)[0] for annot_file in annot_files
+        ]
 
         # Find common names between image and annotation files
-        all_same_names = list(set(image_name_files).intersection(annot_name_files))
+        all_same_names = list(
+            set(image_name_files).intersection(annot_name_files))
 
-        incompatible_files = [image_name for image_name in image_name_files if image_name not in annot_name_files]
+        incompatible_files = [
+            image_name
+            for image_name in image_name_files
+            if image_name not in annot_name_files
+        ]
         if not incompatible_files:
-            print('All are compatible')
+            print("All are compatible")
         else:
             for file_name in incompatible_files:
                 print(f"File '{file_name}' is incompatible.")
-        print('----------')
+        print("----------")
         return all_same_names
 
     def __getitem__(self, index):
         # Capture the image name and the full image path
         image_name = self.all_same_names[index]
         image_path = self.find_image_path(image_name)
-        
+
         # read the image
         image = cv2.imread(image_path)
-        print(image_path, ':', image.shape)
-        # convert BGR to RGB color format
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
+        print(image_name, ":", image.shape)
+
+        # Check if the image is grayscale (single channel)
+        if image.shape[2] == 1:
+            # Convert single-channel grayscale to 3-channel grayscale
+            image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
+        else:
+            # Convert BGR to RGB color format
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        image = image.astype(np.float32)
         image_resized = cv2.resize(image, (self.width, self.height))
         image_resized /= 255.0
 
         # Capture the corresponding XML file for getting the annotations
-        annot_filename = image_name + '.xml'
+        annot_filename = image_name + ".xml"
         annot_file_path = os.path.join(self.annots_dir, annot_filename)
 
         # Get bounding boxes và labels
@@ -69,11 +93,10 @@ class CustDataset(Dataset):
 
         # Apply the label images if has
         if self.transforms:
-            sample = self.transforms(image=image_resized,
-                                     bboxes=target['boxes'],
-                                     labels=labels)
-            image_resized = sample['image']
-            target['boxes'] = torch.Tensor(sample['bboxes'])
+            sample = self.transforms(
+                image=image_resized, bboxes=target["boxes"], labels=labels)
+            image_resized = sample["image"]
+            target["boxes"] = torch.Tensor(sample["bboxes"])
 
         return image_resized, target
 
@@ -89,13 +112,15 @@ class CustDataset(Dataset):
         root = tree.getroot()
         image_width, image_height = image_shape[1], image_shape[0]
 
-        for member in root.findall('object'):
-            labels.append(self.classes.index(member.find('name').text))
-            coords = ['xmin', 'xmax', 'ymin', 'ymax']
+        for member in root.findall("object"):
+            labels.append(self.classes.index(member.find("name").text))
+            coords = ["xmin", "xmax", "ymin", "ymax"]
             # Get the coordinate values of the bounding box
-            xmin, xmax, ymin, ymax = [int(member.find('bndbox').find(coord).text) for coord in coords]
+            xmin, xmax, ymin, ymax = [
+                int(member.find("bndbox").find(coord).text) for coord in coords
+            ]
             # xmin, xmax, ymin, ymax = [int(member.find('bndbox').find(coord).text) for coord in ['xmin', 'xmax', 'ymin', 'ymax']]
-            
+
             # Adjust the bounding box coordinates to the new size
             xmin_final = (xmin / image_width) * self.width
             xmax_final = (xmax / image_width) * self.width
@@ -113,42 +138,41 @@ class CustDataset(Dataset):
             labels = torch.as_tensor(labels, dtype=torch.int64)
 
             target = {
-                'boxes': boxes,
-                'labels': labels,
-                'area': area,
-                'iscrowd': iscrowd
+                "boxes": boxes,
+                "labels": labels,
+                "area": area,
+                "iscrowd": iscrowd,
             }
             image_id = torch.tensor([index])
-            target['image_id'] = image_id
+            target["image_id"] = image_id
 
             return target
         except:
-            print('image ',torch.tensor([index]))
+            print("image ", torch.tensor([index]))
 
     def __len__(self):
         return len(self.all_same_names)
 
-#---------------------------PREPARE DATA-----------------------------------------#
+
+# ---------------------------PREPARE DATA-----------------------------------------#
 
 images_dir = IMAGES_DIR
 all_image_paths = [os.path.join(images_dir, fname)
-                    for fname in os.listdir(images_dir)
-                    if fname.endswith(IMAGE_TYPE)]
+                   for fname in os.listdir(images_dir)
+                   if fname.endswith(IMAGE_TYPE)]
 
-print("Count Image:",len(all_image_paths))
+print("Count Image:", len(all_image_paths))
 
 TRAIN_IMG, TEST_IMG = train_test_split(all_image_paths, test_size=SPLIT_RATIO)
 
 # prepare the final datasets and data loaders
 train_dataset = CustDataset(TRAIN_IMG, IMAGES_DIR, ANNOTS_DIR,
-                              RESIZE_TD[0], RESIZE_TD[1], CLASSES,
-                              get_train_transform())
+                            RESIZE_TD[0], RESIZE_TD[1], CLASSES, get_train_transform())
 valid_dataset = CustDataset(TEST_IMG, IMAGES_DIR, ANNOTS_DIR,
-                              RESIZE_TD[0], RESIZE_TD[1], CLASSES,
-                              get_valid_transform())
+                            RESIZE_TD[0], RESIZE_TD[1], CLASSES, get_valid_transform())
 
-print('train_dataset:', train_dataset)
-print('valid_dataset:', valid_dataset)
+print("train_dataset:", train_dataset)
+print("valid_dataset:", valid_dataset)
 
 train_loader = DataLoader(
     train_dataset,
@@ -173,7 +197,8 @@ print(f"Number of validation samples: {len(valid_dataset)}\n")
 # USAGE: python datasets.py
 if __name__ == "__main__":
     # sanity check of the Dataset pipeline with sample visualization
-    dataset = CustDataset(TRAIN_IMG, IMAGES_DIR, ANNOTS_DIR, RESIZE_TD[0], RESIZE_TD[1], CLASSES)
+    dataset = CustDataset(TRAIN_IMG, IMAGES_DIR, ANNOTS_DIR,
+                          RESIZE_TD[0], RESIZE_TD[1], CLASSES)
     print(f"Number of training images: {len(dataset)}")
 
     # function to visualize a single sample
@@ -181,18 +206,19 @@ if __name__ == "__main__":
     def visualize_sample(image, target, save_path=None):
         try:
             image_copy = image.copy()
-            box = target['boxes'][0]
-            label = CLASSES[target['labels'][0]]
+            box = target["boxes"][0]
+            label = CLASSES[target["labels"][0]]
             cv2.rectangle(image_copy,
                           (int(box[0]), int(box[1])),
                           (int(box[2]), int(box[3])),
-                          (0, 255, 0), 2)  # Removed extra parentheses around the color argument
+                          (0, 255, 0), 2,
+                          )  # Removed extra parentheses around the color argument
 
-            cv2.putText(image_copy, label, (int(box[0]), int(box[1] - 5)),
+            cv2.putText(image_copy, label,
+                        (int(box[0]), int(box[1] - 5)),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-            
             plt.imshow(cv2.cvtColor(image_copy, cv2.COLOR_BGR2RGB))
-            plt.axis('off')
+            plt.axis("off")
             plt.show()
 
             if save_path:
@@ -207,5 +233,5 @@ if __name__ == "__main__":
         idx = np.random.randint(0, len(dataset), size=1)
         image, target = dataset[idx[0]]
         visualize_sample(image, target)
-        print('image.shape:', image.shape)
+        print("image.shape:", image.shape)
         # print(image.shape)
