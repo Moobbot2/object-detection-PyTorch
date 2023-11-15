2232843a97ce8368700d24a2dcae2093619a672e
Ngô ĐứcTâm <133946215+Moobbot2@users.noreply.github.com>
2023-11-15 16:06:47 +0700

    update check_model v0.2

--- src/check_model.py
+++ src/check_model.py
@@ -2,6 +2,7 @@ import os
 import cv2
 from xml.etree import ElementTree as ET
 import sys
+import time
 
 
 def calculate_iou(box1, box2):
@@ -44,8 +45,8 @@ def draw_boxes_on_image(image_path, gt_boxes, pred_boxes):
 
     return image
 
-# Your draw_boxes_on_image function
 
+# Your draw_boxes_on_image function
 
 gt_xml_directory = './dataset/annots/label_test'
 pred_xml_directory = './dataset/test_predictions/xml'
@@ -57,12 +58,13 @@ os.makedirs(output_directory, exist_ok=True)
 gt_boxes = {}
 pred_boxes = {}
 
+
 # Create a log file
-log_file_path = 'history.log'
+log_file_path = f'log/history_{time.time()}.log'
 with open(log_file_path, 'w') as log_file:
     # Redirect stdout to the log file
     sys.stdout = log_file
-    
+
     # Process XML files
     for xml_filename in os.listdir(gt_xml_directory):
         if xml_filename.endswith('.xml'):
@@ -73,25 +75,31 @@ with open(log_file_path, 'w') as log_file:
                 print(f"Ground truth or predicted XML file not found for: {xml_filename}")
                 continue
 
-            gt_boxes[xml_filename] = []
+            gt_boxes[xml_filename] = {'boxes': [], 'names': []}
             tree_gt = ET.parse(gt_xml_file_path)
             root_gt = tree_gt.getroot()
             for obj in root_gt.findall('object/bndbox'):
-                box = [int(obj.find(coord).text) for coord in ['xmin', 'ymin', 'xmax', 'ymax']]
-                gt_boxes[xml_filename].append(box)
+                box = [int(obj.find(coord).text) if obj.find(coord) is not None else 0 for coord in ['xmin', 'ymin', 'xmax', 'ymax']]
+                gt_boxes[xml_filename]['boxes'].append(box)
+                name_element = obj.find('name')
+                name = name_element.text if name_element is not None else ''
+                gt_boxes[xml_filename]['names'].append(name)
 
-            pred_boxes[xml_filename] = []
+            pred_boxes[xml_filename] = {'boxes': [], 'names': []}
             tree_pred = ET.parse(pred_xml_file_path)
             root_pred = tree_pred.getroot()
             for obj in root_pred.findall('object/bndbox'):
-                box = [int(obj.find(coord).text) for coord in ['xmin', 'ymin', 'xmax', 'ymax']]
-                pred_boxes[xml_filename].append(box)
+                box = [int(obj.find(coord).text) if obj.find(coord) is not None else 0 for coord in ['xmin', 'ymin', 'xmax', 'ymax']]
+                pred_boxes[xml_filename]['boxes'].append(box)
+                name_element = obj.find('name')
+                name = name_element.text if name_element is not None else ''
+                pred_boxes[xml_filename]['names'].append(name)
 
             image_name = root_gt.find('filename').text
             image_path = os.path.join(image_directory, image_name)
 
             if os.path.exists(image_path):
-                drawn_image = draw_boxes_on_image(image_path, gt_boxes[xml_filename], pred_boxes[xml_filename])
+                drawn_image = draw_boxes_on_image(image_path, gt_boxes[xml_filename]['boxes'], pred_boxes[xml_filename]['boxes'])
                 if drawn_image is not None:
                     output_path = os.path.join(output_directory, f'{image_name}')
                     cv2.imwrite(output_path, drawn_image)
@@ -100,15 +108,16 @@ with open(log_file_path, 'w') as log_file:
 
     # Calculate IoU and print information
     iou_threshold = 0.1
-    for gt_filename, gt_bounding_boxes in gt_boxes.items():
-        for pred_filename, pred_bounding_boxes in pred_boxes.items():
+    for gt_filename, gt_data in gt_boxes.items():
+        for pred_filename, pred_data in pred_boxes.items():
             if gt_filename == pred_filename:
-                for gt_box in gt_bounding_boxes:
-                    for pred_box in pred_bounding_boxes:
+                for i, gt_box in enumerate(gt_data['boxes']):
+                    for j, pred_box in enumerate(pred_data['boxes']):
                         iou = calculate_iou(gt_box, pred_box)
-                        if iou > iou_threshold:
+                        if iou > iou_threshold and gt_data['names'][i] == pred_data['names'][j]:
                             print(
-                                f"Match found!\nGround Truth XML: {gt_filename}, Bounding Box: {gt_box}\nPredicted XML: {pred_filename}, Bounding Box: {pred_box}\nIoU: {iou}\n")
+                                f"Match found!\nGround Truth XML: {gt_filename}, Bounding Box: {gt_box}, Name: {gt_data['names'][i]}\nPredicted XML: {pred_filename}, Bounding Box: {pred_box}, Name: {pred_data['names'][j]}\nIoU: {iou}\n")
+
 # Restore the original stdout
 sys.stdout = sys.__stdout__
 
